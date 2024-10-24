b64881284a0d7902f73f29fe31b14b01a11d2c7a
madnan-github <dononlineboss@gmail.com>
2024-10-24 14:08:31 +0500

    updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,37 @@
-import Link from "next/link";
+"use client"
+import Link from 'next/link';
+import { useState } from 'react';
 import '@/style/navbar.css';
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    
     <nav className="navbar">
-  <h1 className="navbar-title">Web-Dev</h1>
-  <ul className="navbar-links">
-    <li><Link href="/" className="navbar-link">Home</Link></li>
-    <li><Link href="/about" className="navbar-link">About</Link></li>
-    <li><Link href="/projects" className="navbar-link">Projects</Link></li>
-    <li><Link href="/contact" className="navbar-link">Contact</Link></li>
-  </ul>
+      <h1 className="navbar-title">Web-Dev</h1>
+      <ul className={`navbar-links ${isActive ? 'active' : ''}`}>
+        <li>
+          <Link href="/" className="navbar-link">Home</Link>
+        </li>
+        <li>
+          <Link href="/about" className="navbar-link">About</Link>
+        </li>
+        <li>
+          <Link href="/projects" className="navbar-link">Projects</Link>
+        </li>
+        <li>
+          <Link href="/contact" className="navbar-link">Contact</Link>
+        </li>
+      </ul>
+      <button className="navbar-toggle" onClick={toggleNavbar}>
+        &#9776;
+      </button>
+    </nav>
+  );
+};
 
-  <button className="navbar-toggle">&#9776;</button>
-</nav>
-
-
-  )
-}
-
-export default Navbar
-
+export default Navbar;
