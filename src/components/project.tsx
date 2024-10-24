319b2800a7db7e20f8354c4452fe93ef9387e84c
madnan-github <dononlineboss@gmail.com>
2024-10-24 13:44:19 +0500

    updated

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 import '@/style/project.css';
 
 const projects = [
-  { name: "My Portfolio Website", description: "Number Guessing Game", image: "/img/todoapp.png", url: "https://github.com/madnan-git/myportfolio" },
-  { name: "ATM Program", description: "Simple Command Line ATM Program", image: "/img/todoapp.png", url: "https://github.com/madnan-git/NodeProjects/tree/main/NP_ATM" },
-  { name: "Currency Converter Program", description: "Currency Convertor Program", image: "/img/todoapp.png", url: "https://github.com/madnan-git/NodeProjects/tree/main/NP_CurrencyConverter"  },
-  { name: "Number Guessing Game", description: "Number Guessing Game", image: "/img/todoapp.png", url: "https://github.com/madnan-git/NodeProjects/tree/main/NP_numberGuessingGame" },
-  { name: "Simple Calculator", description: "Simple Calculator", image: "/img/todoapp.png", url: "https://github.com/madnan-git/NodeProjects/tree/main/NP_simpleCalc" },
-  { name: "To Do App", description: "To Do App", image: "/img/todoapp.png", url: "https://github.com/madnan-git/NodeProjects/tree/main/NP_todo" },
+  { name: "My Portfolio Website", description: "My personal website showcasing skills, projects, and contact details.", image: "/img/portfolio.png", url: "https://github.com/madnan-github/portfolio-next-js" },
+  { name: "ATM Program", description: "A Node.js simulation of basic ATM operations like balance inquiry, withdrawal, and deposit.", image: "/img/atm.png", url: "https://github.com/madnan-github/NP_ATM" },
+  { name: "Currency Converter", description: "A tool that converts currencies based on real-time exchange rates.", image: "/img/currency.png", url: "https://github.com/madnan-github/NP_currencyconverter" },
+  { name: "Number Guessing Game", description: "A fun game where users guess a randomly generated number within a given range.", image: "/img/num-game.png", url: "https://github.com/madnan-github/NP_NumberGuessingGame" },
+  { name: "Simple Calculator", description: "Performing basic arithmetic operations like addition, subtraction, multiplication, and division.", image: "/img/calc.png", url: "https://github.com/madnan-github/NP_SimpleCalculator" },
+  { name: "To Do App", description: "A task management app allowing users to add, track, and delete to-do items.", image: "/img/todoapp.png", url: "https://github.com/madnan-github/NP_todo" },
 ];
 
 const Project = () => {
